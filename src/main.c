88010f6337d0f0cecc5d5ef1714e9ab3478216f6
The-KE <kareemelayyan388@gmail.com>
2026-03-02 12:50:19 -0600

    Initialize graphics and services in main.c

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <3ds.h>
+#include <citro2d.h>
+
+int main()
+{
+  // init delta time
+  TickCounter dtt;
+  osTickCounterStart(&dtt);
+  // init services and prep graphic stuff
+  romfsInit();
+  gfxInitDefault();
+  C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
+  C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
+  C2D_Prepare();
+  C3D_RenderTarget* top = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
+  C3D_RenderTarget* btm = C2D_CreateScreenTarget(GFX_BOTTOM, GFX_LEFT);
+}
